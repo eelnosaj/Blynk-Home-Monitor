ea788038844fcbc8a330c1afd825c341a7ce5f3a
eelnosaj <me@jasonlee.id.au>
2017-05-12 19:08:51 +1000

    Add PiTFT buttons and backlight dimming
    Added the beginning of the code that will be used for utilising the
    PiTFT buttons on GPIO 17,22,23,27. For now button 4 (GPIO 17) manually
    toggles the PTII relay.
    Added code to dim the backlight of the PiTFT screen when the PTII relay
    turns the lamp off at night to prevent it turning into a spotlight at
    night.
    Tidied up code to read a little better.

--- loungeroom.py
+++ loungeroom.py
@@ -1,31 +1,48 @@
 import sched
-import time as t
 import sys
+import time as t
 from datetime import date, time, datetime
 import RPi.GPIO as GPIO
 from astral import Astral
 
 s = sched.scheduler(t.time, t.sleep)
 
-GPIO.setmode(GPIO.BCM)
-pin = 30  # GPIO pin connected to the PowerTail Switch II we want to control
-GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
-
-pwm = 18  # Backlight control pin for PiTFT PWM
-GPIO.setup(pwm, GPIO.OUT, initial=GPIO.LOW)
-backlight = GPIO.PWM(18, 1000)
-backlight.start(50)
-
-
 def relay_on():
-    GPIO.output(pin, 1)  # Set pin high to activate the relay
-
+    GPIO.output(30, 1)  # Set pin high to activate the relay
 
 def relay_off():
-    GPIO.output(pin, 0)  # Set the pin low to deactivate the relay
-    backlight.stop()
-    GPIO.cleanup()
+    GPIO.output(30, 0)  # Set the pin low to deactivate the relay
+    backlight.ChangeDutyCycle(20)
     sys.exit()
+
+def button1Pressed(channel):
+        print("Button 1(23) pressed")
+
+def button2Pressed(channel):
+        print("Button 22 pressed")
+
+def button3Pressed(channel):
+        print("Button 27 pressed")
+
+def button4Pressed(channel):
+        print("Button 17 pressed")
+        GPIO.output(30, not GPIO.input(30)) # Toggle GPIO output at pin 30 to activate/deactivate relay
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(30, GPIO.OUT, initial=GPIO.LOW)         # GPIO pin connected to the PowerTail Switch II we want to control
+GPIO.setup(18, GPIO.OUT)                           # Backlight control pin for PiTFT PWM
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # 1
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # 2 PiTFT
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # 3 Buttons
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # 4
+
+GPIO.add_event_detect(23, GPIO.FALLING, callback=button1Pressed, bouncetime=2000)
+GPIO.add_event_detect(22, GPIO.FALLING, callback=button2Pressed, bouncetime=2000)
+GPIO.add_event_detect(27, GPIO.FALLING, callback=button3Pressed, bouncetime=2000)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=button4Pressed, bouncetime=2000)
+
+backlight = GPIO.PWM(18, 1000)
+backlight.start(100)
 
 city_name = 'Sydney'  # City you want Astral to compute solar variables for
 a = Astral()
